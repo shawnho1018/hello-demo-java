eb566347b661096ac5f6d790679305d89ffef8fd
shawnho1018 <shawnho@google.com>
2022-06-12 15:08:02 +0800

    test pre-commit

--- src/main/java/cloudcode/helloworld/HelloWorldApplication.java
+++ src/main/java/cloudcode/helloworld/HelloWorldApplication.java
@@ -17,7 +17,7 @@ public class HelloWorldApplication {
     public static void main(final String[] args) throws Exception {
         String port = System.getenv("PORT");
         if (port == null) {
-            logger.warn("$PORT Environment variable not set");
+            logger.warn("$PORT Environment Variable not Set");
         }
         SpringApplication.run(HelloWorldApplication.class, args);
     }
